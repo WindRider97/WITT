a7f97d3a0fe176b05dc4603cac96f1b6d59ddd6a
WindRider97 <benjamin.mouchet@outlook.com>
2023-01-20 15:26:51 +0100

    fixed post test junit

--- witt_project/src/test/java/ch/hearc/jee2022/witt/PostTest.java
+++ witt_project/src/test/java/ch/hearc/jee2022/witt/PostTest.java
@@ -17,15 +17,19 @@ public class PostTest {
 		Comment comment1 = new Comment("Ah ouais super sympa la loutre");
 		Comment comment2 = new Comment("GoOgLe EsT tOn AmI");
 		Comment comment3 = new Comment("A court d'idées pour le troisième commentaire");
+		
 		comment1.setSavedAt(LocalDateTime.now());
 		comment2.setSavedAt(LocalDateTime.of(2019, 2, 5, 9, 18));
 		comment3.setSavedAt(LocalDateTime.of(2022, 1, 12, 12, 22));
-		comment1.setPost(post);
-		comment2.setPost(post);
-		comment3.setPost(post);
+		
 		comment1.setId(1);
 		comment2.setId(2);
 		comment3.setId(3);
+		
+		post.addComment(comment3);
+		post.addComment(comment2);
+		post.addComment(comment1);
+
 
 		Integer[] expectedCommentId = new Integer[3];
 		int i = 0;
